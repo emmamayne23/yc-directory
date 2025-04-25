f3002a79a09c11f3f75d2c1aa5b73296d33d5c1b
emmamayne23 <emmamayne23@gmail.com>
2025-04-25 19:52:42 -0400

    first commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,14 @@ import Image from "next/image"
 import LogoImage from "@/public/logo.png"
 
 import SignIn from "./SignIn"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
     <nav className="bg-white flex items-center justify-between p-3 md:p-5 text-black">
-        <div>
+        <Link href={"/"}>
             <Image src={LogoImage} alt="Logo" width={100} height={100} className="w-40"/>
-        </div>
+        </Link>
         <div>
             <SignIn />
         </div>
